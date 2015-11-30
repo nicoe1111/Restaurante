ab68b74fbf8d1171a879a7bb787fd9aece19b387
Matias <Matias@MatiPC>
2015-11-30 03:40:16 -0300

    Merge origin/master
    Conflicts:
    	web/Items/Menu.xhtml
    	web/WEB-INF/faces-config.xml
    	web/index.xhtml

--- Class/ClassMySql.php
+++ Class/ClassMySql.php
@@ -51,30 +51,62 @@ function validarUsuario($user, $pass){
                       $Platos[] = $lista;
                 }           	                  
             }else{
-                echo "ERROR: en la consulta con la base de datos";	
-                echo "No pudo cargar el menu de materias";
+                echo "ERROR: en la consulta con la base de datos";
             }
             mysql_close($this->con);
             mysql_free_result($sql);
             return $Platos;  		
     }
     
-function CargarPracticos($id_mat){
-	$menu_practicos = array();
-	$sql = mysql_query("SELECT * FROM practicos WHERE Id_Materia='$id_mat'", $this->con);
+        function getHistorialVentas(){
+            $HistorialVentas = array();
+            $sql = mysql_query("SELECT * FROM historialventas ORDER BY fecha DESC", $this->con);
+            if ($sql){
+                while ($lista = mysql_fetch_array($sql)){
+                      $HistorialVentas[] = $lista;
+                }           	                  
+            }else{
+                echo "ERROR: en la consulta con la base de datos";
+            }
+            mysql_close($this->con);
+            mysql_free_result($sql);
+            return $HistorialVentas;  		
+    }
+    
+    function crearPlato($nombreComida, $precio, $descripcionComida, $categoria, $imagen){
+        $insertarPlato = "INSERT INTO plato(nombre, precio, descripcion, tipo, imagen) VALUES ('$nombreComida','$precio','$descripcionComida','$categoria','$imagen')";
+        $sql = mysql_query($insertarPlato,$this->con);			
+        mysql_close($this->con);
+        return $sql;    
+    }
+    
+function eliminarPlato($id){
+        $deletePlato = "DELETE FROM plato WHERE id_plato='$id'";
+        $sql = mysql_query($deletePlato,$this->con);	
 	if ($sql){
-			while ($lista = mysql_fetch_array($sql)){
-				$menu_practicos[] = $lista;
-			}           	                  
+            echo "Se elimino el plato correctamente";
 	}else{
-		echo "ERROR: en la consulta con la base de datos";	
-		echo "<br />";
-		echo "No pudo cargar los practicos";
+            echo "ERROR: en la consulta con la base de datos";	
 	}
-		mysql_close($this->con);
-		return $menu_practicos;  		
+        mysql_close($this->con);
+        return $sql;
 }
     
+    function getAllUsers(){
+            $Users = array();
+            $sql = mysql_query("SELECT * FROM usuario ORDER BY nombre ASC", $this->con);
+            if ($sql){
+                while ($lista = mysql_fetch_array($sql)){
+                      $Users[] = $lista;
+                }           	                  
+            }else{
+                echo "ERROR: en la consulta con la base de datos";	
+            }
+            mysql_close($this->con);
+            mysql_free_result($sql);
+            return $Users;  		
+    }
+  
 function CargarArchivos($id_Mat, $id_Prac){
 	$menu_archivos = array();
 	$sql = mysql_query("SELECT * FROM archpracticos WHERE Id_Materia='$id_Mat' and Id_Practico= '$id_Prac'", $this->con);
@@ -147,11 +179,71 @@ function InsertarUsuario($nombre, $apellido, $CI, $pass, $tipo){
 			return $sql;
 }
 
-function Insertar_Archivo($IdPractico, $nombreArchivo, $Id_materia, $idUser){
-    
-		$insertarArchivo = "INSERT INTO archpracticos(Nombre, Id_Materia, Id_Usuario, Id_Practico) VALUES ('$nombreArchivo','$Id_materia', '$idUser', '$IdPractico')";
-			$sql = mysql_query($insertarArchivo,$this->con);			
+function FiltrarUsuario($dato){
+            $Users = array();
+            $sql = mysql_query("SELECT * FROM usuario WHERE nombre LIKE '%$dato%' OR apellido LIKE '%$dato%' OR cedula LIKE '%$dato%' OR tipoUser LIKE '%$dato%' ORDER BY cedula ASC", $this->con);
+            if ($sql){
+                while ($lista = mysql_fetch_array($sql)){
+                      $Users[] = $lista;
+                }           	                  
+            }else{
+                echo "ERROR: en la consulta con la base de datos";	
+            }
+            mysql_close($this->con);
+            mysql_free_result($sql);
+            return $Users;  	
+}
+
+function FiltrarComida($dato){
+            $platos = array();
+            $sql = mysql_query("SELECT * FROM plato WHERE nombre LIKE '%$dato%' OR precio LIKE '%$dato%' OR tipo LIKE '%$dato%' ORDER BY id_plato ASC", $this->con);
+            if ($sql){
+                while ($lista = mysql_fetch_array($sql)){
+                      $platos[] = $lista;
+                }           	                  
+            }else{
+                echo "ERROR: en la consulta con la base de datos";	
+            }
+            mysql_close($this->con);
+            mysql_free_result($sql);
+            return $platos;  	
+}
+
+function FiltrarHistorialVenta($dato){
+            $filtro = array();
+            $sql = mysql_query("SELECT * FROM historialventas WHERE fecha LIKE '%$dato%' ORDER BY fecha DESC", $this->con);
+            if ($sql){
+                while ($lista = mysql_fetch_array($sql)){
+                      $filtro[] = $lista;
+                }           	                  
+            }else{
+                echo "ERROR: en la consulta con la base de datos";	
+            }
+            mysql_close($this->con);
+            mysql_free_result($sql);
+            return $filtro;  	
+}
+
+function ModificarUsuario($nombre, $apellido, $cedula, $pass, $tipo){
+		$insertarUnidad = ("UPDATE usuario SET nombre = '$nombre', apellido = '$apellido', tipoUser = '$tipo', pass = '$pass' WHERE cedula = '$cedula'");
+			$sql = mysql_query($insertarUnidad,$this->con);			
 			mysql_close($this->con);
+			return $sql;
+}
+
+function ModificarPlato($id, $nombre, $precio, $descripcion, $tipo, $imagen){
+		$modificarUnidad = ("UPDATE plato SET nombre = '$nombre', precio = '$precio', descripcion = '$descripcion', tipo = '$tipo', imagen = '$imagen' WHERE id_plato = '$id'");
+			$sql = mysql_query($modificarUnidad,$this->con);			
+			mysql_close($this->con);
+                        echo 'iddddd'.$id;
+			return $sql;
+}
+
+function ModificarPlatoSinImagen($id, $nombre, $precio, $descripcion, $tipo){
+		$modificarUnidad = ("UPDATE plato SET nombre = '$nombre', precio = '$precio', descripcion = '$descripcion', tipo = '$tipo' WHERE id_plato = '$id'");
+			$sql = mysql_query($modificarUnidad,$this->con);			
+			mysql_close($this->con);
+                        echo 'iddddd'.$id;
 			return $sql;
 }
     
@@ -188,6 +280,114 @@ function InsertarUnidad($nombre, $idMateria){
 			mysql_close($this->con);
 			return $sql;
 }
+
+function getAllMesas(){
+            $Mesa = array();
+            $sql = mysql_query("SELECT * FROM mesa ORDER BY nombre ASC", $this->con);
+            if ($sql){
+                while ($lista = mysql_fetch_array($sql)){
+                      $Mesa[] = $lista;
+                }           	                  
+            }else{
+                echo "ERROR: en la consulta con la base de datos";	
+            }
+            mysql_close($this->con);
+            mysql_free_result($sql);
+            return $Mesa;  		
+    }
+    
+    function getMesasConPlatos(){
+            $Mesa = array();
+            $sql = mysql_query("SELECT m.* FROM mesa m JOIN mesa_plato mp ON m.id_mesa = mp.id_mesa WHERE mp.conf_cocina is false GROUP BY m.id_mesa ORDER BY m.nombre ASC", $this->con);
+            if ($sql){
+                while ($lista = mysql_fetch_array($sql)){
+                      $Mesa[] = $lista;
+                }           	                  
+            }else{
+                echo "ERROR: en la consulta con la base de datos";	
+            }
+            mysql_close($this->con);
+            mysql_free_result($sql);
+            return $Mesa;  		
+    }
+    
+   function getPlatosMesa($id){
+        $Platos = array();
+        $sql = mysql_query("SELECT * FROM plato p JOIN mesa_plato mp ON p.id_plato = mp.id_plato WHERE mp.id_mesa = ' $id ' ORDER BY precio ASC", $this->con);
+        if ($sql){
+            while ($lista = mysql_fetch_array($sql)){
+                  $Platos[] = $lista;
+            }           	                  
+        }else{
+            echo "ERROR: en la consulta con la base de datos";
+        }
+        mysql_close($this->con);
+        mysql_free_result($sql);
+        return $Platos; 
+    }
+    
+    function getPlatosMesaCocinero($id){
+        $Platos = array();
+        $sql = mysql_query("SELECT * FROM plato p JOIN mesa_plato mp ON p.id_plato = mp.id_plato WHERE mp.id_mesa = '$id' AND mp.conf_cocina is false", $this->con);
+        if ($sql){
+            while ($lista = mysql_fetch_array($sql)){
+                  $Platos[] = $lista;
+            }           	                  
+        }else{
+            echo "ERROR: en la consulta con la base de datos";
+        }
+        mysql_close($this->con);
+        mysql_free_result($sql);
+        return $Platos; 
+    }
+    
+    function setPlatosMesaConfirmar($id){
+        $sql = mysql_query("UPDATE `mesa_plato` SET `conf_cocina` = '1' WHERE `mesa_plato`.`id_mesa_plato` = '$id';", $this->con);
+        mysql_close($this->con);
+        return $sql; 
+    }
+    function CargarPedidos($mozo, $mesa){
+            $Pedidos = array();
+            $sql = mysql_query("SELECT * FROM mesa_plato WHERE Mozo='$mozo' and id_mesa='$mesa' and conf_mozo=false", $this->con);
+            if ($sql){
+                while ($lista = mysql_fetch_array($sql)){
+                      $Pedidos[] = $lista;
+                }           	                  
+            }else{
+                echo "ERROR: en la consulta con la base de datos";	
+                
+            }
+            mysql_close($this->con);
+            mysql_free_result($sql);
+            return $Pedidos;  		
+    }
+    function InsertarPlatoOrden($Plato, $Mesa, $CantidadPlato, $Usuario){
+    
+            $insertarUnidad = "INSERT INTO mesa_plato(id_plato, id_mesa, Cantidad, Mozo) VALUES ('$Plato','$Mesa','$CantidadPlato', '$Usuario')";
+            $sql = mysql_query($insertarUnidad,$this->con);			
+            mysql_close($this->con);
+            return $sql;
+    }
+    function eliminarOrden($id){
+        $deletePlato = "DELETE FROM mesa_plato WHERE id_mesa_plato='$id'";
+        $sql = mysql_query($deletePlato,$this->con);	
+	if ($sql){
+            echo "Se elimino la orden correctamente";
+	}else{
+            echo "ERROR: en la consulta con la base de datos";	
+	}
+        mysql_close($this->con);
+        return $sql;
+    }
+    function terminarOrden($terminar){
+        foreach($terminar as $i){
+            $sql = mysql_query("UPDATE `mesa_plato` SET `conf_mozo` = '1' WHERE `mesa_plato`.`id_mesa_plato` = '$i';", $this->con);
+        }
+        
+        mysql_close($this->con);
+        return $sql; 
+    }
     
 }
+
 ?>
